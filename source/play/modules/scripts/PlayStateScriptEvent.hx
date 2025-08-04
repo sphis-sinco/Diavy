d0dd599d1eca0ca1fd17a3a7976d605101714259
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-04 13:40:43 -0500

    default playstate script event

---
+++ source/play/modules/scripts/PlayStateScriptEvent.hx
@@ -0,0 +1,23 @@
+package play.modules.scripts;
+
+class PlayStateScriptEvent
+{
+	public var eventName:String = 'unimplemented_PSSE';
+	public var eventArgs:Array<Dynamic> = [];
+
+	public function new(name:String = 'unimplemented_PSSE', ?args:Array<Dynamic>)
+	{
+		eventName = name;
+		eventArgs = args ?? [];
+	}
+
+	public function getEventName():String
+	{
+		return eventName;
+	}
+
+	public function getEventArgs():Array<Dynamic>
+	{
+		return eventArgs;
+	}
+}
