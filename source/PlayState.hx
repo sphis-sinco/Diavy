9ad52db6e0949fec7f1b6f2ae5f6da39e50eb495
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-04 11:39:51 -0500

    dialogue variables

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4,6 +4,9 @@ import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	public var dialogue:Array<String> = ["Hey!", "Coolswag. Coolswag."];
+	public var dialogue_progress:Int = 0;
+
 	override public function create()
 	{
 		super.create();
