203cc6d018ac1c0d17f4b0595bdaac557d53eaa8
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-04 12:03:28 -0500

    dialogue text

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4,8 +4,10 @@ import flixel.FlxBasic;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.text.FlxText;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
 
 enum DialoguePositionEnum
 {
@@ -24,6 +26,7 @@ class PlayState extends FlxState
 	public var dialogue_progress:Int = 0;
 
 	public var dialogue_box:FlxSprite;
+	public var dialogue_text:FlxText;
 
 	public var addObject = function(object:FlxBasic) {};
 
@@ -39,7 +42,9 @@ class PlayState extends FlxState
 		dialogue = ['Hey!', 'Coolswag. Coolswag.'];
 
 		initalizePreferences();
+
 		initalizeDialogueBox();
+		initalizeDialogueText();
 
 		beginDialogue();
 	}
@@ -71,6 +76,18 @@ class PlayState extends FlxState
 		dialogue_box.y = FlxG.height - dialogue_box.height - 32;
 		if (preferences.dialoguePosition == TOP)
 			dialogue_box.y = dialogue_box.height * .25;
+	}
+
+	public function initalizeDialogueText()
+	{
+		if (dialogue_text == null)
+		{
+			dialogue_text = new FlxText(0, 0, 0, 'Coolswag Coolswag', 16);
+			addObject(dialogue_text);
+		}
+
+		dialogue_text.color = FlxColor.BLACK;
+		dialogue_text.setPosition(dialogue_box.x + 5, dialogue_box.y + 5);
 	}
 
 	public function initalizePreferences()
