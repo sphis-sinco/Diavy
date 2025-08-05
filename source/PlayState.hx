5d292419c863311af5fcfbf46617e24c58af6f7f
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-04 19:07:33 -0500

    shaved off a good amount of code LOL

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4,9 +4,9 @@ import flixel.addons.text.FlxTypeText;
 import flixel.input.keyboard.FlxKey;
 import flixel.sound.FlxSound;
 import flixel.tweens.*;
-import flixel.util.FlxTimer;
 import play.*;
 import play.dialogue.*;
+import play.modules.choices.ChoicesKeyMap;
 import play.modules.init.*;
 
 class PlayState extends FlxState
@@ -141,6 +141,7 @@ class PlayState extends FlxState
 	}
 
 	public var can_press_enter:Bool = false;
+	public var choices_keys_map:Map<String, FlxKey> = ChoicesKeyMap.default_choices_key_map;
 	public var choices_keys:Array<FlxKey> = [];
 	public var choices_events:Array<String> = [];
 
@@ -340,80 +341,4 @@ class PlayState extends FlxState
 
 		ScriptsManager.callScript(scriptEventNames.beginDialogueTyping);
 	}
-
-	public var choices_keys_map:Map<String, FlxKey> = [
-		// Letters
-		'A' => A,
-		'B' => B,
-		'C' => C,
-		'D' => D,
-		'E' => E,
-		'F' => F,
-		'G' => G,
-		'H' => H,
-		'I' => I,
-		'J' => J,
-		'K' => K,
-		'L' => L,
-		'M' => M,
-		'N' => N,
-		'O' => O,
-		'P' => P,
-		'Q' => Q,
-		'R' => R,
-		'S' => S,
-		'T' => T,
-		'U' => U,
-		'V' => V,
-		'W' => W,
-		'X' => X,
-		'Y' => Y,
-		'Z' => Z,
-		// Numbers (top row)
-		'0' => ZERO,
-		'1' => ONE,
-		'2' => TWO,
-		'3' => THREE,
-		'4' => FOUR,
-		'5' => FIVE,
-		'6' => SIX,
-		'7' => SEVEN,
-		'8' => EIGHT,
-		'9' => NINE,
-		// Symbols
-		'`' => GRAVEACCENT,
-		'-' => MINUS,
-		'[' => LBRACKET,
-		']' => RBRACKET,
-		'\\' => BACKSLASH,
-		';' => SEMICOLON,
-		'\'' => QUOTE,
-		',' => COMMA,
-		'.' => PERIOD,
-		'/' => SLASH,
-		// Whitespace & basic controls
-		' ' => SPACE,
-		'\t' => TAB,
-		'\n' => ENTER,
-		// 'BACKSPACE' => BACKSPACE,
-		'ESCAPE' => ESCAPE,
-		// Function keys
-		'F1' => F1,
-		'F2' => F2,
-		'F3' => F3,
-		'F4' => F4,
-		'F5' => F5,
-		'F6' => F6,
-		'F7' => F7,
-		'F8' => F8,
-		'F9' => F9,
-		'F10' => F10,
-		'F11' => F11,
-		'F12' => F12,
-		// Arrow keys
-		'UP' => UP,
-		'DOWN' => DOWN,
-		'LEFT' => LEFT,
-		'RIGHT' => RIGHT
-	];
 }
