5a93b690fef212b7a9b20dfa310c904753d44490
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-05 11:27:20 -0500

    gonna add asset caching

---
+++ source/CachingState.hx
@@ -0,0 +1,14 @@
+package;
+
+class CachingState extends FlxState
+{
+	override function create()
+	{
+		super.create();
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
