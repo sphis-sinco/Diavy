82af6ec1e2787ff6f37acbf09dfa6622168c1bc3
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-05 11:34:33 -0500

    graphic caching

--- source/CachingState.hx
+++ source/CachingState.hx
@@ -1,5 +1,7 @@
 package;
 
+import flixel.graphics.FlxGraphic;
+
 class CachingState extends FlxState
 {
 	override function create()
@@ -15,7 +17,7 @@ class CachingState extends FlxState
 			{
 				if (file.endsWith(Assets.IMAGE_EXT))
 				{
-					trace('$dir/$file');
+					// trace('$dir/$file');
 					if (!imagePaths.contains('$dir/$file'))
 						imagePaths.push('$dir/$file');
 				}
@@ -28,11 +30,85 @@ class CachingState extends FlxState
 		readDirectory('assets/images');
 		readDirectory(Assets.getAssetPath('images'));
 
+		for (image in imagePaths)
+		{
+			cacheTexture(image);
+		}
+
 		super.create();
 	}
 
 	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
+	}
+
+	static var currentCachedTextures:Map<String, FlxGraphic> = [];
+	static var previousCachedTextures:Map<String, FlxGraphic> = [];
+
+	public static function cacheTexture(key:String):Void
+	{
+		var tpSplit:Array<String>;
+		tpSplit = key.split('/');
+
+		// We don't want to cache the same texture twice.
+		if (currentCachedTextures.exists(key))
+			return;
+
+		if (previousCachedTextures.exists(key))
+		{
+			// Move the graphic from the previous cache to the current cache.
+			var graphic = previousCachedTextures.get(key);
+			previousCachedTextures.remove(key);
+			currentCachedTextures.set(key, graphic);
+			return;
+		}
+
+		// Else, texture is currently uncached.
+		var graphic:FlxGraphic = FlxGraphic.fromAssetKey(key, false, null, true);
+		var fail = graphic == null;
+		if (fail)
+		{
+			FlxG.log.warn('Failed to cache graphic: $key');
+		}
+		else
+		{
+			trace('Successfully cached graphic: $key');
+			graphic.persist = true;
+			currentCachedTextures.set(key, graphic);
+		}
+	}
+
+	/**
+	 * Determine whether the texture with the given key is cached.
+	 * @param key The key of the texture to check.
+	 * @return Whether the texture is cached.
+	 */
+	public static function isTextureCached(key:String):Bool
+	{
+		return FlxG.bitmap.get(key) != null;
+	}
+
+	/**
+	 * Call this, then `cacheTexture` to keep the textures we still need, then `purgeCache` to remove the textures that we won't be using anymore.
+	 */
+	public static function preparePurgeCache():Void
+	{
+		previousCachedTextures = currentCachedTextures;
+		currentCachedTextures = [];
+	}
+
+	public static function purgeCache():Void
+	{
+		// Everything that is in previousCachedTextures but not in currentCachedTextures should be destroyed.
+		for (graphicKey in previousCachedTextures.keys())
+		{
+			var graphic = previousCachedTextures.get(graphicKey);
+			if (graphic == null)
+				continue;
+			FlxG.bitmap.remove(graphic);
+			graphic.destroy();
+			previousCachedTextures.remove(graphicKey);
+		}
 	}
 }
